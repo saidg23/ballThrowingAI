2d723218fee1518cd7128edff0123e65f38b6e5f
Said Garcia <said.garcia12@gmail.com>
2019-03-03 16:34:55 -0600

    minor changes

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,7 @@ let canvas = document.getElementById("canvas");
 let buffer = canvas.getContext("2d");
 drw.bindContext(buffer);
 
-let population = 100;
-let maxIterations = 100;
-let fitnesses = [];
-let successRate = [];
+
 
 function Projectile(x, y)
 {
@@ -21,7 +18,7 @@ function Projectile(x, y)
     this.update = function()
     {
         this.pos = addVectors(this.pos, this.vel)
-        this.vel.y += 0.5;
+        this.vel.y += 0.3;
     }
 }
 
@@ -29,21 +26,33 @@ let projectiles = [];
 
 for(let i = 0; i < population; i++)
 {
-    projectiles.push(new Projectile(getRand(100, 700), getRand(100, 700)));
+    projectiles.push(new Projectile(100, 500));
 }
 
-//testProjectile.launch(15, 15);
+let target = new Vector(getRand(600, 700), getRand(100, 700));
+
+let population = 10;
+let maxIterations = 100;
+let fitnesses = [];
+let successRate = [];
 
 function test()
 {
+    buffer.clearRect(0, 0, canvas.width, canvas.height);
+
+    drw.circle(target, "#ffffff", 10);
+
     for(let i = 0; i < population; i++)
     {
         drw.circle(projectiles[i].pos, "#ffffff", 10);
+        projectiles[i].update();
     }
-    
-    testProjectile.update();
     
     requestAnimationFrame(test);
 }
 
-requestAnimationFrame(test);
+function main()
+{
+}
+
+requestAnimationFrame(main);
